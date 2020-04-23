551e817721cc63ae0e5333c5c8f274550e0ede38
Abhijat Malviya <abhijat@ridecell.com>
2020-04-23 13:47:08 +0530

    export schema in main method

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::fs;
 use std::process;
 
 use crate::field_kinds::FieldKind;
+use crate::object_definitions::ObjectDefinition;
 use crate::schema_parser::Schema;
 
 mod schema_parser;
@@ -22,16 +23,17 @@ fn main() {
     let schema_root = args.nth(1).unwrap();
     let dir = fs::read_dir(schema_root).unwrap();
 
-    let mut reference_map = HashMap::new();
+    let mut reference_map: HashMap<String, ObjectDefinition> = HashMap::new();
 
     for entry in dir {
         let entry = entry.unwrap();
         if entry.file_type().unwrap().is_file() && entry.path().extension().unwrap() == "json" {
             let schema = Schema::from_file(&entry.path()).unwrap();
-            let fullpath: String = entry.file_name().to_string_lossy().to_string();
-            for (name, def) in schema.definitions {
-                reference_map.insert(format!("{}#/definitions/{}", fullpath, name), def);
-            }
+            schema.export_definitions().map(|m| reference_map.extend(m));
         }
+    }
+
+    for (k, v) in reference_map.iter() {
+        println!("{} --> {}", k, v.name);
     }
 }
