3795aa124fc253c20b3b0e3e45bc1250074d56d3
Abhijat Malviya <abhijat@ridecell.com>
2020-04-23 18:18:10 +0530

    breaks down commands into subcommands

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,26 @@ mod field_definitions;
 mod field_kinds;
 mod random_values;
 
+#[derive(Debug, StructOpt)]
+enum Command {
+    /// Print object definitions found and exit
+    Report,
+
+    /// Generate random JSON payloads based on supplied object name
+    Randomize {
+        /// Object name to emit random JSON payloads for
+        #[structopt(short, long)]
+        object_name: String,
+
+        /// Number of random payloads to emit
+        #[structopt(short, long, default_value = "100")]
+        emit_count: u64,
+
+        /// Prettify emitted JSON
+        #[structopt(short, long)]
+        prettify: bool,
+    },
+}
 
 #[derive(Debug, StructOpt)]
 struct Options {
@@ -21,32 +41,46 @@ struct Options {
     #[structopt(short, long)]
     schema_dir: String,
 
-    /// Object name to emit random JSON payloads for
-    #[structopt(short, long)]
-    object_name: String,
-
-    /// Number of random payloads to emit
-    #[structopt(short, long, default_value = "100")]
-    emit_count: u64,
-
-    /// Prettify emitted JSON
-    #[structopt(short, long)]
-    prettify: bool,
+    /// Action to perform
+    #[structopt(subcommand)]
+    command: Command,
 
     /// Exit after showing analyzed object definitions
     #[structopt(short, long)]
-    print_schemas: bool,
+    report_and_exit: bool,
 }
 
 
 fn schema_from_entry(entry: Result<DirEntry>) -> Option<Schema> {
     let entry = entry.ok()?;
     let path = entry.path();
-    
+
     if entry.file_type().ok()?.is_file() && path.extension()? == "json" {
         Schema::from_file(&path)
     } else {
         None
+    }
+}
+
+fn report_schemas(reference_map: &HashMap<String, ObjectDefinition>) {
+    reference_map.iter()
+        .for_each(|(key, object_definition)| {
+            println!("{} in file {}", object_definition.name, key);
+        });
+}
+
+fn randomize_payloads(reference_map: &HashMap<String, ObjectDefinition>, command: &Command) {
+    if let Command::Randomize { object_name, emit_count, prettify } = command {
+        let definition = &reference_map[object_name];
+        for _ in 0..*emit_count {
+            let payload = definition.generate_json(Some(&reference_map)).unwrap();
+            let s = if *prettify {
+                serde_json::to_string_pretty(&payload).unwrap()
+            } else {
+                serde_json::to_string(&payload).unwrap()
+            };
+            println!("{}", s);
+        }
     }
 }
 
@@ -63,14 +97,10 @@ fn main() {
         schema_from_entry(entry).map(|schema| reference_map.extend(schema.export_definitions().unwrap()));
     }
 
-    let definition = &reference_map[&options.object_name];
-    for _ in 0..options.emit_count {
-        let payload = definition.generate_json(Some(&reference_map)).unwrap();
-        let s = if options.prettify {
-            serde_json::to_string_pretty(&payload).unwrap()
-        } else {
-            serde_json::to_string(&payload).unwrap()
-        };
-        println!("{}", s);
+    match &options.command {
+        Command::Report =>
+            report_schemas(&reference_map),
+        Command::Randomize { .. } =>
+            randomize_payloads(&reference_map, &options.command),
     }
 }
