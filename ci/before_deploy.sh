494597b8fbae0804cd217a8c4e5be767d8c82ba6
Abhijat Malviya <abhijat@ridecell.com>
2020-04-24 12:51:07 +0530

    fix build scripts

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -1,5 +1,3 @@
-# This script takes care of building your crate and packaging it for release
-
 set -ex
 
 main() {
@@ -16,12 +14,8 @@ main() {
     esac
 
     test -f Cargo.lock || cargo generate-lockfile
-
-    # TODO Update this to build the artifacts that matter to you
-    cross rustc --bin hello --target $TARGET --release -- -C lto
-
-    # TODO Update this to package the right artifacts
-    cp target/$TARGET/release/hello $stage/
+    cross rustc --bin chimpsky --target $TARGET --release -- -C lto
+    cp target/$TARGET/release/chimpsky $stage/
 
     cd $stage
     tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
